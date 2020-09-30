5648bb5fc36f55ccb680b21fd91531f9fed5548b
UMERSHUJA <52738120+UMERSHUJA@users.noreply.github.com>
2020-09-30 16:55:54 +0500

    Update settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '&)tw$sotxlf%nneuz(jgnayv@cd72a%)%c-(m=x4phg5bswj1o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['herokuappston.herokuapp.com', '127.0.0.1']
 
